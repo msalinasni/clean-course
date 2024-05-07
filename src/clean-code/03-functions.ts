a0a91f7e57ab8ebc78c28a04e41eae86cc35fb89
Miguel Salinas <msalinas@grupo-link.com>
2024-05-07 10:11:32 -0600

    Estado inicial estructura-funciones / Bad

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,5 +36,25 @@
 
     }
 
+    //Continuar
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
+
 
 })();
