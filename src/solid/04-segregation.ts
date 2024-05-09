d0552e8a5382d844382174899ac8d4da64dfc316
Miguel Salinas <msalinas@grupo-link.com>
2024-05-09 10:13:54 -0600

    SOLID - Inicio ISP

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,63 @@
+interface Bird {
+    fly(): void;
+
+    eat(): void;
+
+    run(): void;
+
+    swim(): void;
+}
+
+class Tucan implements Bird{
+
+    public fly(){}
+
+    public eat(){}
+
+    public run(){}
+
+    public swim(){
+        throw new Error('esta ave no nada');
+    }
+}
+    
+class Hummingbird implements Bird{
+
+    public fly(){}
+
+    public eat(){}
+
+    public run(){}
+
+    public swim(){
+        throw new Error('esta ave no nada');
+    }
+}
+
+class Ostrich implements Bird{
+
+    public fly(){
+        throw new Error('esta ave no vuela');
+    }
+
+    public eat(){}
+
+    public run(){}
+
+    public swim(){
+        throw new Error('esta ave no nada');
+    }
+}
+
+class Penguin implements Bird{
+
+    public fly(){
+        throw new Error('esta ave no vuela');
+    }
+
+    public eat(){}
+
+    public run(){}
+
+    public swim(){}
+}
